176a8021f5d75d05eb63a6acee678b88a37be6db
swesh <swesh.mishra123@gmail.com>
2021-09-08 21:41:50 +0530

    Add files via upload

---
+++ dev-challenge-dist/test/spec/AppSpec.js
@@ -0,0 +1,12 @@
+const App = require("../../site/App");
+describe('Testing App', function() {
+    it('App.init() should return App object when App is initialized', function() {
+        const app = App.init();
+        expect(app === App).toBe(true);
+    });
+    it('App.getLiveFeeds() should have been called', function() {
+        spyOn(App,"getLiveFeeds");
+        App.getLiveFeeds();
+        expect(App.getLiveFeeds).toHaveBeenCalled();
+    });
+  });
